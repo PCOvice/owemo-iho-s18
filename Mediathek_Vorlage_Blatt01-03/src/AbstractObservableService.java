60e1cd06e1ac6a40c9d833a6f28b1622d6ae7488
7momenza <7momenza@rzpc79.informatik.uni-hamburg.de>
2018-04-17 13:58:19 +0200

    änderung 2

--- Mediathek_Vorlage_Blatt01-03/src/AbstractObservableService.java
+++ Mediathek_Vorlage_Blatt01-03/src/AbstractObservableService.java
@@ -10,7 +10,7 @@ import java.util.List;
  */
 abstract class AbstractObservableService implements ObservableService
 {
-    //KOMMENTAR
+    //KOMMENTAR2
     /**
      * Die Liste der registrierten Beobachter.
      */
